558549af9191b3a6297703e9e9165a8a8666d1e2
OYEDigitalCorp <131537428+OYEDigitalCorp@users.noreply.github.com>
2023-09-02 14:15:18 +0700

    Update services.js
    Update services.js

--- services.js
+++ services.js
@@ -2,7 +2,7 @@ document.addEventListener('DOMContentLoaded', init, false);
 
 function init() {
     if ('serviceWorker' in navigator && navigator.onLine) {
-        navigator.serviceWorker.register('/service.js')
+        navigator.serviceWorker.register('https://cdn.jsdelivr.net/gh/OYEDigitalCorp/manifest.json@7abad4f4bb009fc3d41aefa8c58ce6cff958b553/services.js')
         .then((reg) => {
             console.log('Registrasi service worker Berhasil', reg);
         }, (err) => {
